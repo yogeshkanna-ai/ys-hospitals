7e98f67ae9e831a918614e5dde059ab0d53b0ef2
yogesh200630 <kannayogesh865@gmail.com>
2026-04-08 00:21:48 +0530

    fixed API URL

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
+  origin: '*',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
